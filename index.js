039f5ea4a9b363432711437b9b3624c0921f32b4
WilliamDASILVA <william.da.silva@outlook.com>
2017-09-15 18:20:45 +0200

    Create browser update wrapper for VueJS

---
+++ index.js
@@ -0,0 +1,47 @@
+import browserUpdate from 'browser-update';
+
+function addLoadEvent(func) {
+  const ondOnLoad = window.onload;
+  if (typeof window.onload !== 'function') {
+    window.onload = func;
+  } else {
+    window.onload = () => {
+      if (oldOnLoad) {
+        oldOnLoad();
+      }
+      func();
+    }
+  }
+}
+
+const VueOutdatedBrowser = {};
+VueOutdatedBrowser.install = (Vue, options) => {
+  const funcs = {
+    show: [],
+    click: [],
+    close: [],
+  };
+  function dispatch(func, infos) {
+    funcs[func].forEach(f => f(infos));
+  }
+  
+  addLoadEvent(() => {
+    options.options.onshow = infos => dispatch('show', infos);
+    options.options.onclick = infos => dispatch('click', infos);
+    options.options.onclose = infos => dispatch('close', infos);
+
+    if (typeof browserUpdate === 'undefined') {
+      throw new Error('The plugin "browser-update" could not be loaded.');
+    } else {
+      browserUpdate(options.options, options.test);
+    }
+  });
+
+  Vue.outdatedBrowser = {
+    onshow: func => funcs.show.push(func),
+    onclick: func => funcs.click.push(func),
+    onclose: func => funcs.close.push(func),
+  };
+};
+
+export default VueOutdatedBrowser;
